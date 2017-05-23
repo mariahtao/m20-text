a67e93314fd0647a66a342d6f12dc84991fddd80
Mariah Tao <mytao@uw.edu>
2017-05-23 14:44:12 -0700

    exervise 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -23,16 +23,31 @@ most.lines <- books %>%
   summarize(lines = n())
 
 # Use the unnest_tokens function to generate the full list of words
-
+full.list <- books %>%
+  unnest_tokens(word, text)
 
 # Which words are most common (regardless of which book them come from)?
-
+word.count <- full.list %>% 
+  group_by(word) %>% 
+  summarize(count = n()) %>% 
+  arrange(-count)
 
 # Remove stop words by performing an anti_join with the stop_words dataframe
-
+remove.stop.words <- full.list %>% 
+  anti_join(stop_words, by="word")
 
 # Which non stop-words are most common?
-
+non.stop.words <- remove.stop.words %>%
+  group_by(word) %>% 
+  summarize(count = n()) %>% 
+  arrange(-count)
 
 # Use ggplot to make a horizontal bar chart of the word frequencies of non-stop words
-
+remove.stop.words %>% 
+  count(word, sort = TRUE) %>%
+  filter(n > 600) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip()
